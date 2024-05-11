0d0c56ae47a94d22e6e106c22506a142b0f2b196
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-11 21:52:41 +0200

    chore: update snap

--- test/detect.test.ts
+++ test/detect.test.ts
@@ -1,22 +1,22 @@
 import { expect, it } from 'vitest'
 
 const cache = [null, true, false] as const
-const deps = [true, false] as const
+const deps = [false, true] as const
 const isTS = [true, false] as const
-const isNativeTsImportSupported = [true, false] as const
+const isNativeTsImportSupported = [false, true] as const
 const isRuntimeSupportsTsx = [true, false] as const
 
 function f(v: boolean | null) {
-  return v == null ? '?' : v ? 'T' : 'F'
+  return v == null ? '❓' : v ? '✅' : '❌'
 }
 
-for (const c of cache) {
-  for (const d of deps) {
-    for (const t of isTS) {
-      for (const n of isNativeTsImportSupported) {
-        for (const r of isRuntimeSupportsTsx) {
+for (const r of isRuntimeSupportsTsx) {
+  for (const c of cache) {
+    for (const d of deps) {
+      for (const t of isTS) {
+        for (const n of isNativeTsImportSupported) {
           it(
-            `cache=${f(c)} | deps=${f(d)} | isTS=${f(t)} | nativeTS=${f(n)} | tsx=${f(r)}`,
+            `tsx=${f(r)} | cache=${f(c)} | deps=${f(d)} | isTS=${f(t)} | nativeTS=${f(n)}`,
             async () => {
               const { detectLoaderLogic } = await import('../src/detect')
               const result = detectLoaderLogic(c, d, t, n, r)
